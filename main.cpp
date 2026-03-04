feabe1692f4946a68d4c50c5d8f0c255744eb652
lucaswoodman <lucastwoodman@gmail.com>
2026-03-03 22:56:43 -0800

    idk

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "User.h"
+#include "Bid.h"
+#include "listings.h"
+
+namespace CSEN79 {
+
+    
+
+    int main() {
+        std::cout << "start" << std::endl;
+
+        std::vector<std::string> log;
+        User* users[10];
+        for (int i = 0; i < 10; i++) {
+            char c = 'a' + i;
+            users[i] = new User(std::string(1, c));
+        }
+
+        for (auto user : users) {
+            user->print();
+        }
+
+        std::cout << "end" << std::endl;
+        return 0;
+    }
+
+}
